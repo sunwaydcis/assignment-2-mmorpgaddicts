c252ac11453f8420984e98cc907afa483c1a1094
ilovecarbss <121186197+ilovecarbss@users.noreply.github.com>
2024-11-28 18:03:22 +0800

    Update Main_Q1.hs
    average by state q1 donezo

--- app/Main_Q1.hs
+++ app/Main_Q1.hs
@@ -10,41 +10,43 @@ main = do
     input <- fetchData
     let formattedData = formatData input
     let groupedByState = groupDataByState formattedData
-    let bedsByState = map calculateTotalBeds groupedByState
-    putStrLn "Total Beds by State:"
-    mapM_ print bedsByState
+    let averagesByState = map calculateAverages groupedByState
+    putStrLn "Average Admissions by State:"
+    mapM_ print averagesByState
 
 -- Fetches the data from the hospital CSV file
 fetchData :: IO String
 fetchData = readFile "data/hospital.csv"
 
 -- Formats raw CSV data
-formatData :: String -> [(String, Double)]
+formatData :: String -> [(String, (Double, Double, Double))]
 formatData raw =
     let 
         rows = lines raw           -- Splits into rows
         rawData = drop 1 rows      -- Drops the header row
         splitRows = map (splitOn ",") rawData -- Splits each row by commas
-        extracted = mapMaybe extractStateAndBeds splitRows
+        extracted = mapMaybe extractStateAndCategories splitRows
     in 
         extracted
 
--- Extracts state and bed counts
-extractStateAndBeds :: [String] -> Maybe (String, Double)
-extractStateAndBeds row =
+-- Extracts state and category data
+extractStateAndCategories :: [String] -> Maybe (String, (Double, Double, Double))
+extractStateAndCategories row =
     case row of
-        (_:state:beds:_) -> case readMaybe beds of
-                              Just beds' -> Just (state, beds')
-                              _          -> Nothing
+        (_:state:_:_:_:admitted_pui:admitted_covid:admitted_total:_) ->
+            case (readMaybe admitted_pui, readMaybe admitted_covid, readMaybe admitted_total) of
+                (Just pui, Just covid, Just total) -> Just (state, (pui, covid, total))
+                _ -> Nothing
         _ -> Nothing
 
 -- Groups data by state
-groupDataByState :: [(String, Double)] -> [[(String, Double)]]
+groupDataByState :: [(String, (Double, Double, Double))] -> [[(String, (Double, Double, Double))]]
 groupDataByState = groupBy (\(s1, _) (s2, _) -> s1 == s2) . sortOn fst
 
--- Calculates total beds for each state
-calculateTotalBeds :: [(String, Double)] -> (String, Double)
-calculateTotalBeds rows =
+-- Calculates averages for each state
+calculateAverages :: [(String, (Double, Double, Double))] -> (String, (Double, Double, Double))
+calculateAverages rows =
     let state = fst (head rows)
-        totalBeds = sum (map snd rows)
-    in (state, totalBeds)
+        (puiSum, covidSum, totalSum) = foldr (\(_, (pui, covid, total)) (p, c, t) -> (p + pui, c + covid, t + total)) (0, 0, 0) rows
+        count = fromIntegral (length rows)
+    in (state, (puiSum / count, covidSum / count, totalSum / count))
